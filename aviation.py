a79e34e0aecaef1112f4334baaac4e62a3a0ba74
Abubakr Hussien <bukihussien@gmail.com>
2023-07-11 22:46:09 +0000

    Added tourism and aviation file

---
+++ aviation.py
@@ -0,0 +1,32 @@
+import requests
+
+api_key = '0592d0699fcfdbc310e9a112b7deb3b9'
+url = 'http://api.aviationstack.com/v1/flights'
+
+# Get user input for departure and arrival airports
+#departure_airport = input("Enter the departure airport: ")
+#arrival_airport = input("Enter the arrival airport: ")
+
+departure_airport = "KLAX"
+arrival_airport = "KIAD"
+
+# Set the required parameters
+params = {
+    'access_key': api_key,
+    'dep_icao': departure_airport,
+    'arr_icao': arrival_airport,
+    'limit': 5
+}
+
+# Make the API request
+response = requests.get(url, params=params)
+
+# Get the JSON response
+data = response.json()
+
+# Access the flight data
+flights = data['data']
+#print(flights)
+
+for flight in flights:
+    print("This is the departure" , flight['departure']['airport'], " and this is the arrival", flight['arrival']['airport'] )
---
+++ tourism.py
@@ -0,0 +1,49 @@
+import requests
+
+def get_locations_to_explore(destination):
+    api_key = '5ae2e3f221c38a28845f05b6fabd118a2607fe089a18dc2906b76ddb' 
+
+    #URL to request the lat and lon of destination
+    url2 = 'https://api.opentripmap.com/0.1/en/places/geoname'
+
+    param1 = {
+        'name' : destination,
+        'apikey': api_key
+    }
+    response1 = requests.get(url2, params=param1)
+    data1 = response1.json()
+    #Stores the lat and lon
+    dest_lat = data1['lat']
+    dest_lon = data1['lon']
+
+    print("This is the data", data1)
+    print("this is lat ", dest_lat, " and lon ", dest_lon )
+
+    #URL to request the locations for the destination using lat and lon
+    url = 'https://api.opentripmap.com/0.1/en/places/radius'
+
+    params = {
+        'radius': '5000',  # Set the radius within which to search for locations (in meters)
+        'lon': dest_lon,  # Replace with the longitude of your destination
+        'lat': dest_lat,  # Replace with the latitude of your destination
+        'kinds': 'interesting_places',  # Specify the kind of places to retrieve
+        'apikey': api_key
+    }
+    try:
+        response = requests.get(url, params=params)
+        data = response.json()
+
+        if 'error' in data:
+            error_message = data['error']['message']
+            print(f"Error: {error_message}")
+        else:
+            locations = data['features']
+            return locations
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+    response = requests.get(url, params=params)
+    data = response.json()
+    #print(data)
+
