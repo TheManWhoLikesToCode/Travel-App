ed82005633520dac4caa7c5f274325ebe3e1c7a5
Abubakr Hussien <bukihussien@gmail.com>
2023-07-13 20:00:39 +0000

    Fixed error

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,6 +1,6 @@
 import unittest, sys
 
-sys.path.append('../../Travel-App') # imports python file from parent directory
+sys.path.append('../Travel-App') # imports python file from parent directory
 from app import app #imports flask app object
 
 class BasicTests(unittest.TestCase):
