ecb296c8810b46576609e2d608ab3acbbe514cd9
Jaydin_MacBook <JaydinFreemanWork@gmail.com>
2023-07-14 11:39:50 -0400

    Reverted tourism.py

--- tourism.py
+++ tourism.py
@@ -1,34 +1,14 @@
-from typing import List, Dict, Any
 import requests
-from requests.exceptions import RequestException
+from typing import List, Dict, Any
 
 API_KEY = '5ae2e3f221c38a28845f05b6fabd118a2607fe089a18dc2906b76ddb'
 
 GEONAME_API = 'https://api.opentripmap.com/0.1/en/places/geoname'
 RADIUS_API = 'https://api.opentripmap.com/0.1/en/places/radius'
 
-PROXY = 'http://themanwholikestocode.pythonanywhere.com/'
-proxies = {'http': PROXY, 'https': PROXY}
-
-
-def get_geoname_data(destination: str) -> Dict[str, Any]:
-
-    print("Getting geoname data for:", destination)
-
-    params = {'name': destination, 'apikey': API_KEY}
-
-    try:
-        response = requests.get(GEONAME_API, params=params, proxies=proxies)
-        geoname_data = response.json()
-        print("Geoname API response:", geoname_data)
-        return geoname_data
-
-    except RequestException as e:
-        print("Error getting geoname data:", e)
-        return {}
-
 
 def get_locations_to_explore(destination: str) -> List[Dict[str, Any]]:
+    """Get interesting locations around a destination."""
 
     # Input validation
     if not destination:
@@ -39,42 +19,46 @@ def get_locations_to_explore(destination: str) -> List[Dict[str, Any]]:
         print("Destination must be a string")
         return []
 
-    # Get geoname data
-    geoname_data = get_geoname_data(destination)
-
-    # Extract lat and lon
-    lat = geoname_data.get('lat')
-    lon = geoname_data.get('lon')
-
-    # Define Radius params
-    radius_params = {
-        'radius': '5000',
-        'lat': lat,
-        'lon': lon,
-        'kinds': 'interesting_places',
+    # Geoname API
+    params = {
+        'name': destination,
         'apikey': API_KEY
     }
 
-    # Radius API call
     try:
-        print("Calling Radius API")
-        response = requests.get(
-            RADIUS_API, params=radius_params, proxies=proxies)
-        radius_data = response.json()
+        response = requests.get(GEONAME_API, params=params)
+        print(response)
+        data = response.json()
 
-        print("Radius API response:", radius_data)
+        destination_lat = data['lat']
+        destination_lon = data['lon']
 
-        if 'error' in radius_data:
-            print("Radius API error:", radius_data['error'])
+    except requests.exceptions.RequestException as e:
+        print(f"Geoname API request failed: {e}")
+        return []
+
+    # Radius API
+    params = {
+        'radius': '5000',
+        'lon': destination_lon,
+        'lat': destination_lat,
+        'kinds': 'interesting_places',
+        'apikey': API_KEY,
+        'rate': 3,
+        'limit': 20
+    }
+
+    try:
+        response = requests.get(RADIUS_API, params=params)
+        data = response.json()
+
+        if 'error' in data:
+            print(f"Error from API: {data['error']['message']}")
             return []
 
-        if 'features' not in radius_data:
-            print("No features found")
-            return []
-
-        locations = radius_data['features']
+        locations = data['features']
         return locations
 
-    except RequestException as e:
-        print("Radius API request failed:", e)
+    except requests.exceptions.RequestException as e:
+        print(f"Radius API request failed: {e}")
         return []
